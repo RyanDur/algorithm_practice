9ddce3f386e6629fb23edffbb7cac38925f0cc64
RyanDur <ryan.p.durling@gmail.com>
2013-08-03 19:11:10 -0700

    my container: just doing some things the hard way

---
+++ My_Container.rb
@@ -0,0 +1,57 @@
+class My_Container
+
+  def initialize num
+    @my_array = Array.new(num)
+    if !num.is_a? Array
+      @my_array.each_index{|i| @my_array[i] = i}
+    end
+  end
+
+  def size
+    @my_array.size
+  end
+
+  def insert num
+    @my_array.push num
+  end
+
+  def insert_at num, index
+    @my_array.push(nil)
+    (@my_array.size-1).downto(index+1).each do |i|
+      @my_array[i] = @my_array[i-1]
+    end
+    @my_array[index] = num
+  end
+
+  def contains? num
+    result = false
+    @my_array.each do |i|
+      if (i == num)
+        return result = true
+      end
+    end
+    result
+  end
+
+  def get_at index
+    @my_array[index]
+  end
+
+  def delete_at index
+    value = @my_array[index]
+    @my_array[index] = nil
+    (index..@my_array.size-2).each do |num|
+      @my_array[num] = @my_array[num+1]
+    end
+    @my_array.pop
+    value
+  end
+
+  def print
+    puts '---------'
+    @my_array.each_index do |i|
+      puts "| #{i} | #{@my_array[i]} |"
+      puts '---------'
+    end
+  end
+end
---
+++ spec/My_Container_Spec.rb
@@ -0,0 +1,113 @@
+require 'spec_helper'
+
+describe 'My_Container' do
+
+  before :each do
+    @container = My_Container.new(10)
+  end
+
+  describe 'initialize with number' do
+    it 'should create a container of the size given' do
+      @container.size.should eq(10)
+    end
+
+    it 'should not leave any position within it nil' do
+      @container.contains?(nil).should be_false
+    end
+
+    it 'should populate the container with numbers from 0 to the size-1' do
+      for num in 0..@container.size-1
+        @container.contains?(num).should be_true
+      end
+    end
+  end
+
+  describe 'initialize with array' do
+    it "should create an instance with the array provided" do
+      container = My_Container.new([1,1,1])
+      container.should be_an_instance_of My_Container
+      container.size.should eq(3)
+
+      for num in 0..container.size-1
+        container.get_at(num).should eq(1)
+      end
+    end
+  end
+
+  describe 'insert' do
+    it "should insert a value into the container" do
+      @container.contains?(11).should be_false
+      @container.insert(11)
+      @container.contains?(11).should be_true
+    end
+  end
+
+  describe 'size' do
+    it 'should return te size of the container' do
+      @container.size.should eq(10)
+      @container.size.should_not eq(9)
+    end
+  end
+
+  describe 'contains?' do
+    it 'should return true if the container contains the number' do
+      @container.contains?(5).should be_true
+      @container.contains?(9).should be_true
+      @container.contains?(3).should be_true
+    end
+
+    it 'should return true if the container contains the number' do
+      @container.contains?(15).should be_false
+      @container.contains?(29).should be_false
+      @container.contains?(33).should be_false
+    end
+  end
+
+  describe 'delete_at' do
+    it 'should delete the value at the given index' do
+      @container.contains?(3).should be_true
+      @container.delete_at(3)
+      @container.contains?(3).should be_false
+    end
+
+    it 'should close the gap created by the deletion by shifting the contents down' do
+      @container.delete_at(3)
+      @container.contains?(nil).should be_false
+      @container.size.should eq(9)
+    end
+
+    it 'should return the value deleted' do
+      @container.delete_at(5).should eq(5)
+    end
+  end
+
+  describe 'get_at' do
+    it 'it should return the value at the given index' do
+      @container.get_at(0).should eq(0)
+      @container.get_at(5).should eq(5)
+    end
+  end
+
+  describe 'insert_at' do
+    it 'should insert the number given at the index given' do
+      @container.insert_at(15,3)
+      @container.get_at(3).should eq(15)
+    end
+
+    it 'should shift the contents up to accomodate the insertion' do
+      ary = [1,5,2,7,5]
+      con = My_Container.new(ary)
+      con.size.should eq(5)
+
+      index = 2
+      con.insert_at(33,index)
+      con.get_at(1).should eq(5)
+      con.size.should eq(6)
+      con.contains?(nil).should be_false
+
+      (index..ary.size-1).each do |num|
+        con.get_at(num+1).should eq(ary[num])
+      end
+    end
+  end
+end

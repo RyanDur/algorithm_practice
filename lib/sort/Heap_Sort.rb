26f7af518db29275e74c017b6a589d6231257e2c
RyanDur <ryan.p.durling@gmail.com>
2013-08-16 20:30:31 -0700

    refactor heap sort

--- lib/sort/Heap_Sort.rb
+++ lib/sort/Heap_Sort.rb
@@ -1,47 +1,44 @@
 class Heap_Sort
 
   def sort collection
-    @heap = collection
-    @size = collection.size-1
-    heapify
-    sequentialize
-    @heap
+    to_sequence heapify collection
   end
 
   private
-  def sequentialize last = @size
-    return if last <= 0
-    swap 0, last
-    sort_down 0, last-1
-    sequentialize last-1
+  def to_sequence collection, last = collection.size-1
+    return collection if last <= 0
+    swap collection, 0, last
+    sort_down collection, 0, last-1
+    to_sequence collection, last-1
   end
 
-  def heapify parent = @size/2
-    return if parent < 0
-    sort_down parent
-    heapify parent-1
+  def heapify collection, parent = collection.size-1/2
+    return collection if parent < 0
+    sort_down collection, parent
+    heapify collection, parent-1
   end
 
-  def swap parent, child
-    @heap[parent], @heap[child] = @heap[child], @heap[parent]
+  def swap collection, parent, child
+    collection[parent], collection[child] = collection[child], collection[parent]
   end
 
-  def sort_down parent, heap_end = @size
+  def sort_down collection, parent, heap_end = collection.size-1
     left_child = 2*parent + 1
     right_child = left_child + 1
 
+    return collection if left_child > heap_end
+
     if right_child <= heap_end and
-      @heap[right_child] > @heap[parent] and
-      @heap[left_child] < @heap[right_child]
+      collection[left_child] < collection[right_child] and
+      collection[right_child] > collection[parent]
 
-      swap parent, right_child
-      sort_down right_child, heap_end
+      swap collection, parent, right_child
+      sort_down collection, right_child, heap_end
+    end
 
-    elsif left_child <= heap_end and
-      @heap[parent] < @heap[left_child]
-
-      swap parent, left_child
-      sort_down left_child, heap_end
+    if collection[parent] < collection[left_child]
+      swap collection, parent, left_child
+      sort_down collection, left_child, heap_end
     end
   end
 end

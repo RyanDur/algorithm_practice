432479f54466443aee17f3e4ec90b5119dcb8dd7
RyanDur <ryan.p.durling@gmail.com>
2013-08-20 12:23:54 -0700

    testing works for liear search

--- app/spec/javascripts/search/LinearSearchSpec.js
+++ app/spec/javascripts/search/LinearSearchSpec.js
@@ -4,30 +4,43 @@ describe('LinearSearch', function() {
   beforeEach(function() {
     searchList = [5,3,4,2,1,6,9,8,0,7];
     setFixtures($("<div class='demo'></div>"));
-    ls = new LinearSearch(searchList);
-    ls.insertInto($('.demo').get(0));
+    ls = new LinearSearch($('.demo').get(0), searchList);
     jasmine.Clock.useMock();
   });
 
-  xdescribe('insertInto', function() {
+  describe('init', function() {
     it('should insert the elements into the dom', function() {
       expect($('.collection > li')).toHaveLength(searchList.length);
     });
   });
 
-  xdescribe('search', function() {
+  describe('search', function() {
     it('should search for a given value', function() {
       ls.search(8);
-      jasmine.Clock.tick(11);
+      jasmine.Clock.tick(10000);
       expect($('.index7')).toHaveClass('found');
 
       ls.search(1);
+      jasmine.Clock.tick(10000);
       expect($('.index4')).toHaveClass('found');
     });
 
     it('should only have one search class in the list', function() {
-      //      ls.search(8);
-      //      expect($('.index7')).toHaveClass('found');
+      ls.search(8);
+      jasmine.Clock.tick(7000);
+      expect($('.index0')).not.toHaveClass('found');
+      expect($('.index1')).not.toHaveClass('found');
+      expect($('.index2')).not.toHaveClass('found');
+      expect($('.index3')).not.toHaveClass('found');
+      expect($('.index4')).not.toHaveClass('found');
+      expect($('.index5')).not.toHaveClass('found');
+      expect($('.index6')).not.toHaveClass('found');
+
+      expect($('.index7')).toHaveClass('found');
+      expect($('.index7')).toHaveClass('search');
+
+      expect($('.index8')).not.toHaveClass('found');
+      expect($('.index9')).not.toHaveClass('found');
     });
   });
 });

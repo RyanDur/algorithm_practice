6f991e1a72e5fa9d762d71b8dc657095ec9f590e
RyanDur <ryan.p.durling@gmail.com>
2013-08-20 00:17:57 -0700

    binary search testing

--- app/spec/javascripts/search/BinarySearchSpec.js
+++ app/spec/javascripts/search/BinarySearchSpec.js
@@ -5,8 +5,7 @@ describe('BinarySearch', function() {
   beforeEach(function() {
     searchList = [0,1,2,3,4,5,6,7,8,9];
     setFixtures($("<div class='demo'></div>"));
-    bs = new BinarySearch(searchList);
-    bs.insertInto($('.demo').get(0));
+    bs = new BinarySearch($('.demo').get(0));
   });
 
   describe('insertInto', function() {
@@ -16,25 +15,85 @@ describe('BinarySearch', function() {
   });
 
   describe('search', function() {
+    beforeEach(function() {
+      jasmine.Clock.useMock();
+    });
+
     it('should repeatedly cut the list in half ignoring the half the value does not exist', function() {
       bs.search(8);
+      for(var i = 0; i < 10; i++) {
+        expect($('.index' + i)).not.toHaveClass('found');
+        expect($('.index' + i)).not.toHaveClass('search');
+        expect($('.index' + i)).not.toHaveClass('ignore');
+      }
+      jasmine.Clock.tick(2000);
+      for(var i = 0; i < 10; i++) {
+        expect($('.index' + i)).not.toHaveClass('found');
+      }
+      expect($('.index0')).not.toHaveClass('ignore');
+      expect($('.index1')).not.toHaveClass('ignore');
+      expect($('.index2')).not.toHaveClass('ignore');
+      expect($('.index3')).not.toHaveClass('ignore');
+
+      expect($('.index4')).toHaveClass('search');
+
+      expect($('.index5')).not.toHaveClass('ignore');
+      expect($('.index6')).not.toHaveClass('ignore');
+      expect($('.index7')).not.toHaveClass('ignore');
+      expect($('.index8')).not.toHaveClass('ignore');
+
+      jasmine.Clock.tick(2000);
+      for(var i = 0; i < 10; i++) {
+        expect($('.index' + i)).not.toHaveClass('found');
+      }
+      expect($('.index0')).toHaveClass('ignore');
+      expect($('.index1')).toHaveClass('ignore');
+      expect($('.index2')).toHaveClass('ignore');
+      expect($('.index3')).toHaveClass('ignore');
+
+      expect($('.index4')).toHaveClass('ignore');
+      expect($('.index4')).not.toHaveClass('search');
+
+      expect($('.index5')).not.toHaveClass('ignore');
+      expect($('.index6')).not.toHaveClass('ignore');
+
+      expect($('.index7')).toHaveClass('search');
+      expect($('.index7')).not.toHaveClass('ignore');
+
+      expect($('.index8')).not.toHaveClass('ignore');
+      expect($('.index9')).not.toHaveClass('ignore');
+
+      jasmine.Clock.tick(2000);
       expect($('.index0')).toHaveClass('ignore');
       expect($('.index1')).toHaveClass('ignore');
       expect($('.index2')).toHaveClass('ignore');
       expect($('.index3')).toHaveClass('ignore');
       expect($('.index4')).toHaveClass('ignore');
       expect($('.index5')).toHaveClass('ignore');
+      expect($('.index6')).toHaveClass('ignore');
+
+      expect($('.index7')).toHaveClass('ignore');
+      expect($('.index7')).not.toHaveClass('search');
+
+      expect($('.index8')).not.toHaveClass('ignore');
+      expect($('.index8')).toHaveClass('search');
+      expect($('.index8')).toHaveClass('found');
+
+      expect($('.index9')).not.toHaveClass('ignore');
     });
 
     it('should mark the search target as found', function() {
       bs.search(4);
+      jasmine.Clock.tick(8000);
       expect($('.index4')).toHaveClass('found');
 
       bs.search(6);
+      jasmine.Clock.tick(8000);
       expect($('.index6')).toHaveClass('found');
       expect($('.index6')).not.toHaveClass('ignore');
 
       bs.search(9);
+      jasmine.Clock.tick(8000);
       expect($('.index9')).toHaveClass('found');
       expect($('.index9')).not.toHaveClass('ignore');
     });
